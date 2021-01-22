b2e7b3381296ee92e1bb99b0a03777a6378cfcb1
Thato Rammoko <technifyaf@gmail.com>
2021-01-22 19:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function logWhisper(string){
 }
 
 sayHiToGrandma(string){
-  if (string === "I love you, Grandma"){
-    return "I love you, too"
+  switch (string) {
+    case "I love you, Grandma" : return "I love you too"
+    case : string === shout(string): return "YES INDEED"
+    case : string === whisper(string): return "I can't hear you"
+    default:return;
+      
   }
-
-  
-
   }
